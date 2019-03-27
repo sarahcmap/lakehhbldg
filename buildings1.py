e110e6309de29c33c3b2d085f7e78da2118174f8
sarahcmap <sbuchhorn@cmap.illinois.gov>
2019-03-27 16:59:49 -0500

    improvements

--- buildings1.py
+++ buildings1.py
@@ -25,39 +25,29 @@ lakehh = hh[hh.maz.isin(lakecountymaz)]
 hh = pd.read_csv("C:/Users/sbuchhorn/Desktop/2010pop/buildings/babydata/hh10149.csv")
 bldg = pd.read_csv("C:/Users/sbuchhorn/Desktop/2010pop/buildings/babydata/bldg10149.csv")
 
-class building:
-    # property: id 'TEMP_BLDG_ID'
-    # property: maz 'subzone17'
-    # property: taz 'zone17'
-    'year_built'
-    # year built 'classyear'
-    # building type 'building_type_id'
-    # res units 'residential_units'
-    'totalEstValue'
-    'classacre'
-    'bedroomest'
-    'classbldg'
 
-class household:
-    # property: id 'household_id'
-    # property: maz 'maz'
-    # property: taz 'taz'
-    # pool of choices: output of function build pool
-    # assigned building: output of function assign building
-    # property value? 'VALP'
-    # business? 'BUS'
-    'ACR'
-    'BDSP'
-    'CONP'
-    'YBL'
+class Household:
+    def __init__(self, ID, p, bID, score):
+        self.hhID = ID
+        self.pickOrder = p
+        self.buildingID = bID
+        self.score = score
+
 
 hhids = [x for x in hh.household_id]
 
+
+#result dictionary will be like household_id : [pick order, building id, score]
 # get random hh
 chooserHH = random.choice(hhids)
-
+# get options in maz
 bldgoptions = maz_filter(chooserHH)
-
+# rate options
+ratedBldgOptions = rate_pool(chooserHH, bldgoptions)
+# select
+selectedID = assign_building(ratedBldgOptions)
+# assign and update
+removeAndUpdate(bldg, hh, chooserHH,selectedID)
 
 
 def maz_filter(household):
@@ -85,25 +75,72 @@ def maz_filter_remainder(LISTA):
 
 
 
-def rate_pool(household, location_pool):
-    score_type()
-    score_year()
-    score_value()
+def rate_pool(household, bldgoptions):
+    bldgoptions['choiceScore'] = 0
 
-    return rated_pool
+    rowhh = hh[hh['household_id'] == household]
 
-def score_type(typea, typeideal):
-def score_year():
-def score_value():
+    hhtype = rowhh.BLD.values[0]
+    hhyear = rowhh.YBL.values[0]
+    hhbus = rowhh.BUS.values[0]
+    hhbroom = rowhh.BDSP.values[0]
+    hhvalp = rowhh.VALP.values[0]
+    hhcond = rowhh.CONP.values[0]
+    hhacre = rowhh.ACR.values[0]
+
+    # score type
+    bldgoptions.loc[bldgoptions['classbldg'] == hhtype, 'choiceScore'] += 5
+
+    # score year
+    bldgoptions.loc[bldgoptions['classyear'] == hhyear, 'choiceScore'] += 2
+    bldgoptions.loc[bldgoptions['classyear'] == hhyear +- 1, 'choiceScore'] += 1
+
+    # score condo
+    if hhcond > 0:
+        bldgoptions.loc[bldgoptions['building_type_id'] == 1210, 'choiceScore'] += 1
+
+    # single family attributes
+    if hhtype in [1,2,3]:
+        # business
+        if hhbus == 1.0:
+            bldgoptions.loc[bldgoptions['building_type_id'].isin([2130,2110,2120]), 'choiceScore'] += 2
+        # bedrooms (estimated for one unit buildings only)
+        bldgoptions.loc[(bldgoptions['residential_units'] == 1) &
+                        (bldgoptions['bedroomsest'] == hhbroom), 'choiceScore'] += 2
+        bldgoptions.loc[(bldgoptions['residential_units'] == 1) &
+                        (bldgoptions['bedroomsest'] == hhbroom +- 1), 'choiceScore'] += 1
+        # lot size
+        bldgoptions.loc[bldgoptions['classacre'] == hhacre, 'choiceScore'] += 2
+        # value (for one unit buildings only)
+        bldgoptions.loc[(bldgoptions['residential_units'] == 1) &
+                        (hhvalp - 5000 < bldgoptions['totalEstValue']) &
+                        (bldgoptions['totalEstValue'] < hhvalp + 5000), 'choiceScore'] += 2
+
+
+    return bldgoptions
 
 
 
 def assign_building(rated_pool):
     # make a selection given the weights
-    removeAndUpdate()
+    rankedPool = rated_pool.sort_values('choiceScore',ascending=False).reset_index()
+    # for initial testing, give them top choice
+    selectedID = rankedPool.iloc[0]['TEMP_BLDG_ID']
 
-def removeAndUpdate():
+    return selectedID
+
+
+def removeAndUpdate(buildingdf, hhdf, hhID, bldgID):
     # decrease the selected building's res_units count by one or remove building if 0
+    hhdf[hhdf['household_id'] == hhID]['building_id'] = bldgID
+    hhdf = hhdf[~hhdf['household_id'] == hhID]
+    units = buildingdf[buildingdf['TEMP_BLDG_ID'] == bldgID]['residential_units'].values[0]
+    if units == 1:
+        buildingdf = buildingdf[~buildingdf['TEMP_BLDG_ID'] == bldgID]
+    else:
+        buildingdf.loc[buildingdf['TEMP_BLDG_ID'] == bldgID, 'residential_units'] = units - 1
+
+    return buildingdf, hhdf
 
     return updated buildings
 

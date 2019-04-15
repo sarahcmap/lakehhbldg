d554952d51f84b79d650264ddd7a9fa8f0b9323e
sarahcmap <sbuchhorn@cmap.illinois.gov>
2019-04-15 09:01:55 -0500

    -third choice -res units fix -acre and value point changes

--- gettestdata.py
+++ gettestdata.py
@@ -5,11 +5,11 @@
 """
 import pandas as pd
 bldgs = pd.read_csv("C:/Users/sbuchhorn/Desktop/2010pop/buildings/current/lakebuildingsextraatts_2.csv")
-hh = pd.read_csv("C:/Users/sbuchhorn/Desktop/2010pop/buildings/lakehh_v3.csv")
+hh = pd.read_csv("C:/Users/sbuchhorn/Desktop/2010pop/buildings/current/lakehh_v3.csv")
 
 maz_selections = [10254, 10259]
 taz_selections = [2327, 2350, 2353]
-puma = 3303
+puma = 3301
 
 bldgstest2 = bldgs[bldgs['subzone17'].isin(maz_selections)]
 hhtest2 = hh[hh['maz'].isin(maz_selections)]
@@ -20,5 +20,5 @@ hhtest2 = hh[hh['taz'].isin(taz_selections)]
 bldgstest2 = bldgs[bldgs['puma5'] == puma]
 hhtest2 = hh[hh['puma'] == puma]
 
-bldgstest2.to_csv("C:/Users/sbuchhorn/Desktop/2010pop/buildings/babydata/bldg6.csv",index=False)
-hhtest2.to_csv("C:/Users/sbuchhorn/Desktop/2010pop/buildings/babydata/hh6_3.csv", index=False)
+bldgstest2.to_csv("C:/Users/sbuchhorn/Desktop/2010pop/buildings/babydata/bldg3301.csv",index=False)
+hhtest2.to_csv("C:/Users/sbuchhorn/Desktop/2010pop/buildings/babydata/hh3301.csv", index=False)

75f496ea235cdd57faecf098bc97e51a74b9dc27
sarahcmap <sbuchhorn@cmap.illinois.gov>
2019-09-11 15:49:27 -0500

    improvements WIP

--- buildings1.py
+++ buildings1.py
@@ -4,7 +4,7 @@
 """
 author: sbuchhorn
 date: 28 Mar 2019
-updated: 16 April 2019
+updated: 11 Sept 2019
 description: assign synthetic households to buildings in Lake County
 """
 
@@ -13,12 +13,19 @@ import random
 import numpy as np
 import time
 import sys
+import os
 
+# end with slash!
+outputdir = "T:/buchhorn/buildingtesting/test3/"
+
+# check that output dirs are ok
+if not os.path.exists(outputdir):
+    sys.stderr.write("output folder doesn't exist! \n fix before continuing")
 
 # TEST DATA
-fullhh = pd.read_csv("T:/buchhorn/buildingtesting/hh3301.csv")
+fullhh = pd.read_csv("T:/buchhorn/buildingtesting/hhtest3.csv")
 fullhh = fullhh[fullhh['BLD'] != 10]    # exclude households not in buildings (boat, rv, van, etc)
-fullbldg = pd.read_csv("T:/buchhorn/buildingtesting/bldg3301.csv")
+fullbldg = pd.read_csv("T:/buchhorn/buildingtesting/bldgtest3.csv")
 fullbldg['remaining_residential_units'] = fullbldg['residential_units']
 
 neartaz = pd.read_csv("T:/buchhorn/buildingtesting/sz17neighbor.csv")
@@ -36,7 +43,6 @@ def maz_filter(household, unmatched):
     bldgoptions = bldg[(bldg['subzone17'] == hhmaz) & (bldg['remaining_residential_units'] > 0)].copy()
 
     if len(bldgoptions) == 0:
-        sys.stderr.write('no maz houses')
         unmatched.append(household)
         return bldgoptions, unmatched
 
@@ -51,7 +57,6 @@ def tazFilter(household):
     bldgoptions = bldg[(bldg['zone17'] == hhtaz) & (bldg['remaining_residential_units'] > 0)].copy()
 
     if len(bldgoptions) == 0:
-        sys.stderr.write('no taz houses')
         return bldgoptions
 
     else:
@@ -65,7 +70,6 @@ def tazNeighborFilter(household):
     bldgoptions = bldg[(bldg['zone17'].isin(neighborDict[hhtaz])) & (bldg['remaining_residential_units'] > 0)].copy()
 
     if len(bldgoptions) == 0:
-        sys.stderr.write('no neighbor taz houses')
         return bldgoptions
 
     else:
@@ -91,34 +95,41 @@ def rate_pool(household, bldgoptions):
 
     rowhh = hh.loc[hh['household_id'] == household]
 
-    # determine top quartile once based only on type and year
-    hhlevel1 = rowhh.LEVEL1.values[0]
-    level1colname = "score_{}".format(hhlevel1)
-    bldgoptions.loc[:, 'choiceScore'] += bldgoptions[level1colname]
+    # determine top quartile once based only on type
+    hhtype = rowhh.BLD.values[0]
+    scorecol = 'score{}'.format(hhtype.astype(int))
+    bldgoptions['choiceScore'] = bldgoptions[scorecol]
 
     bldgoptions = bldgoptions.sort_values('choiceScore',ascending=False)
     # get list of unique scores and get top quartile cutoff
     topquartile = np.percentile(bldgoptions.choiceScore.unique(), 75)
     # take only rows from df where score is greater or equal to this cutoff
     bldgoptions = bldgoptions[bldgoptions['choiceScore'] >= topquartile]
+    # max 100 options
+    if len(bldgoptions) > 100:
+        bldgoptions = bldgoptions.sample(100)
 
     # add detail as appropriate
     hhcond = rowhh.CONP.values[0]
     # score condo
     if hhcond > 0:
-        bldgoptions.loc[bldgoptions['building_type_id'] == 1210, 'choiceScore'] += 1
+        bldgoptions.loc[bldgoptions['condo'] == 1, 'choiceScore'] += 1  # condo, hotel, or office building
 
-    hhtype = rowhh.BLD.values[0]
     hhbus = rowhh.BUS.values[0]
     hhacre = rowhh.ACR.values[0]
     hhbroom = rowhh.BDSP2.values[0]
     hhvalp = rowhh.VALP.values[0]
+    hhyear = rowhh.YBL2.values[0]
+
+    # score year
+    yscorecol = 'yscore{}'.format(hhyear.astype(int))
+    bldgoptions.loc[:,'choiceScore'] += bldgoptions[yscorecol]
 
     # single family attributes
     if hhtype in [1,2,3]:
         # business
         if hhbus == 1.0:
-            bldgoptions.loc[bldgoptions['building_type_id'].isin([2130,2110,2120]), 'choiceScore'] += 1
+            bldgoptions.loc[bldgoptions['building_type_id'].isin([2130, 2110, 2120]), 'choiceScore'] += 1
         # lot size
         bldgoptions.loc[bldgoptions['classacre'] == hhacre, 'choiceScore'] += 1
 
@@ -197,6 +208,9 @@ def matchHouseholds(hhids, unmatched, bldg, hhdf):
     for i in hhids:
         pickorder += 1
 
+        if pickorder % 10000 == 0:
+            sys.stdout.write('working on hh {}'.format(pickorder))
+
         # get options in maz
         bldgoptions, unmatched = maz_filter(i, unmatched)
 
@@ -211,7 +225,7 @@ def matchHouseholds(hhids, unmatched, bldg, hhdf):
             resultdf.loc[i, ['pickorder','bldgid','score','topscore']] = [pickorder, selectedID, score, topscore]
 
     end = time.time()
-    sys.stderr.write('maz level done. Took {} seconds'.format(end-start))
+    sys.stdout.write('\n maz level done. Took {} seconds'.format(end-start))
 
     return resultdf, unmatched, pickorder
 
@@ -265,36 +279,44 @@ def matchRemainder(unmatched, resultdf, pickorder, bldg, hhdf):
                     sys.stderr.write('hh cannot be matched')
                     failed.append(i)
 
-    sys.stderr.write('all done')
     end = time.time()
-    sys.stderr.write('Remainder matching done. Took {} seconds'.format(end-start))
+    sys.stdout.write('\n Remainder matching done. Took {} seconds'.format(end-start))
 
     return resultdf, failed
 
 
 #flow
 for i in puma:
-    print("working on puma ()".format(i))
+    print("working on puma {}".format(i))
     hh = fullhh[fullhh['puma'] == i]
-    bldg = fullbldg[fullbldg['puma'] == i]
+    bldg = fullbldg[fullbldg['puma5'] == i]
 
     resultdf, unmatched, failed, hhids = setup(hh)
     mazresult, unmatched, pickorder = matchHouseholds(hhids=hhids, unmatched=unmatched, bldg=bldg, hhdf=hh.copy())
     finalresult, failed = matchRemainder(unmatched=unmatched, resultdf=mazresult, pickorder=pickorder, bldg=bldg, hhdf=hh)
 
-    hhwid = hh.merge(finalresult,left_on='household_id',right_on=finalresult.index)
-    allinfo = hhwid.merge(bldg, right_on='building_id', left_on='bldgid')
-    allinfo = allinfo[['household_id','maz','subzone17','taz','zone17','puma',
-             'BUS','CONP','BLD','classbldg', 'building_type_id', 'remaining_residential_units','residential_units',
-             'VALP','totalEstValue','improvement_value',
-             'YBL2','classyear','year_built', #land_value
-             'RMSP','BDSP2','bedroomsest','residential_sqft',
-             'ACR','classacre','acres',
-             'pickorder','bldgid','score','topscore']]
-
-    allinfo.to_csv("T:/buchhorn/buildingtesting/r_{}.csv".format(i), index=False)
-    dfnotmatched = allinfo[allinfo['BLD'] != allinfo['classbldg']]
-    a = dfnotmatched.groupby(['subzone17','BLD']).classbldg.value_counts().rename('count').reset_index()
-    a.to_csv("T:/buchhorn/buildingtesting/mismatch_{}.csv".format(i), header=['maz','originaltype','newtype','count'], index=False)
+    hh.to_csv(outputdir + "hh_{}.csv".format(i))
+    finalresult.to_csv(outputdir + "finalresult_{}.csv".format(i))
+    bldg.to_csv(outputdir + "bldg_{}.csv".format(i))
     seriesfailed = pd.Series(failed)
-    seriesfailed.to_csv("T:/buchhorn/buildingtesting/failed_{}.csv".format(i))
+    seriesfailed.to_csv(outputdir + "kanefailed_{}.csv".format(i))
+
+
+
+    #
+    # hhwid = hh.merge(finalresult,left_on='household_id',right_on=finalresult.index)
+    # allinfo = hhwid.merge(bldg, right_on='building_id', left_on='bldgid')
+    # allinfo = allinfo[['household_id','maz','subzone17','taz','zone17','puma',
+    #          'BUS','CONP','BLD','classbldg', 'building_type_id', 'remaining_residential_units','residential_units',
+    #          'VALP','totalEstValue','improvement_value',
+    #          'YBL2','classyear','year_built', #land_value
+    #          'RMSP','BDSP2','bedroomsest','residential_sqft',
+    #          'ACR','classacre','acres',
+    #          'pickorder','bldgid','score','topscore']]
+    #
+    # allinfo.to_csv(outputdir + "r_{}.csv".format(i), index=False)
+    # dfnotmatched = allinfo[allinfo['BLD'] != allinfo['classbldg']]
+    # a = dfnotmatched.groupby(['subzone17','BLD']).classbldg.value_counts().rename('count').reset_index()
+    # a.to_csv(outputdir + "mismatch_{}.csv".format(i), header=['maz','originaltype','newtype','count'], index=False)
+    # seriesfailed = pd.Series(failed)
+    # seriesfailed.to_csv(outputdir + "failed_{}.csv".format(i))
